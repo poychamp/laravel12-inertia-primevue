bad0b87b387dd8de207d77b30516c52ec4e625c7
DESKTOP-I3USAS7\PC <champoyradoo@gmail.com>
2025-04-23 00:16:21 +0800

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: '172.27.128.1'
+        },
+    },
+
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
